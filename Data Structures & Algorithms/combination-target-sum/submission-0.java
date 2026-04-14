ba7a4ad645d262f55876e77674cadfde3e9625a4
AryaASinhaS <arya__sinha@outlook.com>
2026-04-14 23:25:32 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> combinationList = combinationGroups(nums, 0, target);
+        return combinationList;
+    }
+
+    public List<List<Integer>> combinationGroups(int[] nums, int index, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(target == 0)
+        {
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+
+        if(index >= nums.length || target < 0)
+        {
+            return result;
+        }
+
+        List<List<Integer>> val1 = new ArrayList<>();
+        List<List<Integer>> val2 = new ArrayList<>();
+
+        //Case 1 : Current Index is considered, we make RC on i
+        //(same number can be reconsidered) with target = target - nums[i]
+        val1 = combinationGroups(nums, index, target - nums[index]);
+
+        //Case 2 : Current Index is not considered, we make RC on i + 1
+        //with target = target
+        val2 = combinationGroups(nums, index + 1, target);
+
+        for(List<Integer> curr : val1)
+        {
+            curr.add(nums[index]);
+            result.add(curr);
+        }
+
+        for(List<Integer> curr : val2)
+        {
+            result.add(curr);
+        }
+
+        return result;
+    }
+}
