6d16620acc199dae66f69e74ba23d6cb98b2a3a8
AryaASinhaS <arya__sinha@outlook.com>
2026-04-19 21:43:39 +0530

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+
+        return formCombinations(n,n);
+        
+    }
+
+    public List<String> formCombinations(int open, int close)
+    {
+        List<String> result = new ArrayList<>();
+
+       if(open == 0 && close == 0)
+       {
+            result.add("");
+            return result;
+       }
+
+       if(open > 0)
+       {
+            List<String> currResult = formCombinations(open - 1, close);
+            result.addAll(formString("(", currResult));
+            
+       }
+
+       if(close > open)
+       {
+            List<String> currResult = formCombinations(open, close - 1);
+            result.addAll(formString(")", currResult));
+       }
+       return result;
+    }
+
+    public List<String> formString(String bracket, List<String> currResult)
+    {
+        List<String> result = new ArrayList<>();
+        for(String s : currResult)
+        {
+            String newS = bracket + s;
+            result.add(newS);
+        }
+        return result;
+    }
+}
