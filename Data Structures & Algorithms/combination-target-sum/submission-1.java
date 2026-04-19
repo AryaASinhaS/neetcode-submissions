0f1ba90ffc133e2df1a980949a201846a5f6d09f
AryaASinhaS <arya__sinha@outlook.com>
2026-04-19 14:38:35 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+/**
+IP: Integer Array, target
+OP: List<List<Integer>> showing combinations where sum of each = target
+Edge Case: Empty Array
+Approach:
+At each step we see if we have to choose the given no of array or not
+If we choose we reduce the target by that no. and also append the no. to our answer list
+If we dont we ignore the no. and answer remains same
+**/
+
+public List<List<Integer>> combinationSum(int[] nums, int target) {
+	
+	return findAllcombinationSum(nums, target, 0);
+	
+}
+
+public List<List<Integer>> findAllcombinationSum(int[] nums, int target, int index) {
+
+	List<List<Integer>> result = new ArrayList<>();
+
+	if(target == 0)
+	{
+		result.add(new ArrayList<>());
+		return result;
+	}
+
+	if(index >= nums.length || target < 0)
+	{
+		return result;
+
+	}
+
+	//case 1 - considering the element
+	List<List<Integer>> val1 = findAllcombinationSum(nums, target - nums[index], index);
+	for(List<Integer> curr : val1)
+    {
+	    curr.add(nums[index]);
+        result.add(curr);
+    }
+
+//case 2 - not considering the element
+	List<List<Integer>> val2 = findAllcombinationSum(nums, target, index + 1);
+    for(List<Integer> curr : val2)
+    {
+        result.add(curr);
+    }
+
+	return result;
+
+	
+	
+}
+
+
+
+}
