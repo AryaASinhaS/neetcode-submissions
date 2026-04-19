0e5f3f3fb5efb5aa07dd2c0ff0c731aa9809f94d
AryaASinhaS <arya__sinha@outlook.com>
2026-04-19 19:27:21 +0530

    Add: subsets-ii - submission-6

---
+++ Data Structures & Algorithms/subsets-ii/submission-6.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        /**
+        IP: Integer Array
+        OP: List<List<Integer>> WITH ALL SUBSETS
+        Approach:
+        start with a list that is empty, then for each next number iterate over results and add current number to each sub lists
+        **/
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+	List<Integer> empty = new ArrayList<>();
+	result.add(empty);
+
+	//code to get all subsets
+	for(int n : nums)
+{
+	List<List<Integer>> currResult = new ArrayList<>();
+
+	for(List<Integer> curr : result)
+	{
+		List<Integer> temp = new ArrayList<>(curr);
+		temp.add(n);
+		currResult .add(temp);
+	}
+
+	for(List<Integer> curr : currResult )
+	{
+		result .add(curr);
+	}
+}
+	return removeDuplicates(result);
+
+}
+
+public List<List<Integer>> removeDuplicates(List<List<Integer>> result) 
+{
+
+	HashSet<List<Integer>> hs = new HashSet<>(result);
+	return new ArrayList<>(hs);
+
+}
+
+}
