9bb8a67df73b76c2ec02161c0f86ca67aa271e58
AryaASinhaS <arya__sinha@outlook.com>
2026-05-20 20:14:05 +0530

    Add: find-k-closest-elements - submission-2

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-2.java
@@ -0,0 +1,69 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] ar, int k, int x) {
+        /**
+        IP: Sorted array, int k - count, int x - target
+        OP: list of k closest integers of x
+        Edge case: empty array
+        Approach:
+        use BS to find LB of x
+        move 1 position left and right of LB and pick the numbers which are closer to x
+        **/
+        List<Integer> result = new LinkedList<>();
+
+        if(ar.length == 0)
+            return result;
+        int lowerBound = findLowerBound(ar, x), left = lowerBound, right = left+1, count = 0;
+
+        while(count < k && left >= 0 && right < ar.length)
+        {
+            if(Math.abs(x-ar[left]) <= Math.abs(x-ar[right]))
+            {
+                result.addFirst(ar[left]);
+                left--;
+            }
+            else
+            {
+                result.add(ar[right]);
+                right++;
+            }
+            count++;
+        }
+
+        while(count < k && right == ar.length)
+        {
+            result.addFirst(ar[left]);
+            left--;
+            count++;
+        }
+
+        while(count < k && left < 0)
+        {
+            result.add(ar[right]);
+            right++;
+            count++;
+        }
+        return result;
+    }
+
+    public int findLowerBound(int[] ar, int target)
+    {
+        int l = 0, r = ar.length-1, mid = 0, potentialLB = 0;
+
+        while(l<=r)
+        {
+            mid = (l+r)/2;
+            if(ar[mid] == target)
+                return mid;
+            else if(ar[mid] < target)
+            {
+                potentialLB = mid;
+                l = mid+1;
+            }
+            else
+            {
+                r = mid-1;
+            }
+        }
+        return potentialLB;
+    }
+}
