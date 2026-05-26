35af475312e5d457ae8b4c30ef53b0b45a6cec40
AryaASinhaS <arya__sinha@outlook.com>
2026-05-26 15:06:09 +0530

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        /**
+		at each iteration, branch into checking 2 scenes, one where we will consider nums[i] again
+		another where we wont
+		if we reach end of array return back
+		**/
+
+		return findAllCombinations(nums, 0, target);
+    }
+
+	public List<List<Integer>> findAllCombinations(int[] nums, int startIndex, int target)
+	{
+		List<List<Integer>> result = new ArrayList<>();
+
+		if(target == 0)
+		{
+			result.add(new ArrayList<>());
+			return result;
+		}
+
+		if(startIndex >= nums.length || target < 0)
+			return result;
+		
+		//consider the current nums
+		List<List<Integer>> val1 = findAllCombinations(nums, startIndex, target - nums[startIndex]);
+		for(List<Integer> c : val1)
+		{
+			c.add(nums[startIndex]);
+			result.add(c);
+		}
+
+		//do not consider the current nums
+		List<List<Integer>> val2 = findAllCombinations(nums, startIndex + 1, target);
+		for(List<Integer> c : val2)
+		{
+			result.add(c);
+		}
+		return result;
+	}
+}
