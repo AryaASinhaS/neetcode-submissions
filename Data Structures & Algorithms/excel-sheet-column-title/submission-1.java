e7fe97ae5e93e78b1c8c949499573edf5548a88e
AryaASinhaS <arya__sinha@outlook.com>
2026-06-23 13:34:44 +0530

    Add: excel-sheet-column-title - submission-1

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String convertToTitle(int columnNumber) 
+    {
+        HashMap<Integer, Character> hm = new HashMap<>();
+        String result = "";
+
+        for(int i = 1; i < 27; i++)
+        {
+            hm.put(i, (char)(64 + i));
+        }
+
+        while(columnNumber > 26)
+        {
+            int val = columnNumber % 26;
+            
+            if(val > 0)
+            {
+                System.out.println("val = " + val + " hm.get(val) = " + hm.get(val));
+                result = hm.get(val) + result;
+            }
+               
+            columnNumber = columnNumber / 26;
+        }
+
+        result = hm.get(columnNumber) + result;
+        return result;
+    }
+}
