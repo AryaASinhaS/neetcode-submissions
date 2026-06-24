0a5e4676503d0b5bcedee92f8b58046ee74f9308
AryaASinhaS <arya__sinha@outlook.com>
2026-06-24 19:32:31 +0530

    Add: longest-palindromic-substring - submission-10

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-10.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String longestPalindrome(String s) 
+    {
+        int len = s.length();
+        String updatedStr = "" + s.charAt(0);
+        for(int i = 1; i < len; i++)
+        {
+            updatedStr = updatedStr + '#' + s.charAt(i);
+        }
+
+        String currPalindrome = "", maxPalindrome = "", result = "";
+        int pos = 1;
+
+        len = updatedStr.length();
+
+        for(int i = 1; i < len; i++)
+        {
+            pos = 1;
+            while((i - pos) >= 0 && (i + pos) < len && updatedStr.charAt(i - pos) == updatedStr.charAt(i + pos))
+            {   
+                pos++;
+            }
+
+            pos--;
+            currPalindrome = updatedStr.substring(i-pos, i+pos+1);
+            //System.out.println("currPalindrome = " + currPalindrome);
+            if(currPalindrome.charAt(0) != '#' && currPalindrome.length() == maxPalindrome.length())
+            {
+                maxPalindrome = currPalindrome;
+            }   
+            if(currPalindrome.length() > maxPalindrome.length())
+            {
+                maxPalindrome = currPalindrome;
+            }   
+        }
+
+        for(int i = 0; i < maxPalindrome.length(); i++)
+        {
+            if(maxPalindrome.charAt(i) != '#')
+            {
+                result = result + maxPalindrome.charAt(i);
+            }
+        }
+
+        return (result != "" ? result : "" + s.charAt(0));
+    }
+}
